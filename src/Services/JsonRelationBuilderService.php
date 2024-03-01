01074a23c5cc146b9d043190e3a5dcd145423481
Drawsider <drawsider@gmail.com>
2024-03-01 14:34:39 +0300

    Refactor JsonRelationBuilderService to dynamically extend parent class with JsonRelationTrait.

--- src/Services/JsonRelationBuilderService.php
+++ src/Services/JsonRelationBuilderService.php
@@ -2,10 +2,30 @@
 
 namespace Shigabutdinoff\JsonRelationBuilder\Services;
 
-use \Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Builder;
 use Shigabutdinoff\JsonRelationBuilder\Traits\JsonRelationTrait;
 
-class JsonRelationBuilderService extends Builder
+class JsonRelationBuilderService
 {
-    use JsonRelationTrait;
+    public function model(string $parentClass)
+    {
+        $trait = JsonRelationTrait::class;
+        $dynamicClass = basename($parentClass);
+        $code = "class $dynamicClass extends $parentClass {use $trait;}";
+        eval($code);
+        return new $dynamicClass();
+    }
+
+    public function builder(Builder $builder)
+    {
+        $trait = JsonRelationTrait::class;
+        $parentClass = $builder::class;
+        $dynamicClass = basename($parentClass);
+        $code = "class $dynamicClass extends $parentClass {use $trait;}";
+        eval($code);
+        return (new $parentClass/*->setQuery*/($builder->getQuery()))
+            ->setModel($builder->getModel())
+            ->setEagerLoads($builder->getEagerLoads())
+            ->setBindings($builder->getBindings());
+    }
 }
